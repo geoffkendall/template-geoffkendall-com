939c4bb2ce8454d17c3a4bf9e330ec88a11d0f8d
geoffkendall <github@geoffkendall.com>
2023-05-19 13:23:39 +0100

    Initial commit

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import { getPostBySlug, getAllPosts, getLinksMapping } from '../lib/api'
-import { markdownToHtml } from '../lib/markdownToHtml'
-import type PostType from '../interfaces/post'
-import path from 'path'
-import PostSingle from '../components/blog/post-single'
-import Layout from '../components/misc/layout'
-import { NextSeo } from 'next-seo'
+import { useRouter } from "next/router"
+import ErrorPage from "next/error"
+import { getPostBySlug, getAllPosts, getLinksMapping } from "../lib/api"
+import { markdownToHtml } from "../lib/markdownToHtml"
+import type PostType from "../interfaces/post"
+import path from "path"
+import PostSingle from "../components/blog/post-single"
+import Layout from "../components/misc/layout"
+import { NextSeo } from "next-seo"
 
 type Items = {
-  title: string,
-  excerpt: string,
+  title: string
+  excerpt: string
 }
 
 type Props = {
@@ -37,13 +37,17 @@ export default function Post({ post, backlinks }: Props) {
             openGraph={{
               title: post.title,
               description,
-              type: 'article',
-              images: [{
-                url: (post.ogImage?.url) ? post.ogImage.url : "https://fleetingnotes.app/favicon/512.png",
-                width: (post.ogImage?.url) ? null: 512,
-                height: (post.ogImage?.url) ? null: 512,
-                type: null
-              }]
+              type: "article",
+              images: [
+                {
+                  url: post.ogImage?.url
+                    ? post.ogImage.url
+                    : "https://geoff.li/favicon/android-chrome-512x512.png",
+                  width: post.ogImage?.url ? null : 512,
+                  height: post.ogImage?.url ? null : 512,
+                  type: null,
+                },
+              ],
             }}
           />
           <PostSingle
@@ -69,21 +73,27 @@ type Params = {
 export async function getStaticProps({ params }: Params) {
   const slug = path.join(...params.slug)
   const post = await getPostBySlug(slug, [
-    'title',
-    'excerpt',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
+    "title",
+    "excerpt",
+    "date",
+    "slug",
+    "author",
+    "content",
+    "ogImage",
   ])
-  const content = await markdownToHtml(post.content || '', slug)
+  const content = await markdownToHtml(post.content || "", slug)
   const linkMapping = await getLinksMapping()
-  const backlinks = Object.keys(linkMapping).filter(k => linkMapping[k].includes(post.slug) && k !== post.slug)
-  const backlinkNodes = Object.fromEntries(await Promise.all(backlinks.map(async (slug) => {
-    const post = await getPostBySlug(slug, ['title', 'excerpt']);
-    return [slug, post]
-  })));
+  const backlinks = Object.keys(linkMapping).filter(
+    (k) => linkMapping[k].includes(post.slug) && k !== post.slug
+  )
+  const backlinkNodes = Object.fromEntries(
+    await Promise.all(
+      backlinks.map(async (slug) => {
+        const post = await getPostBySlug(slug, ["title", "excerpt"])
+        return [slug, post]
+      })
+    )
+  )
 
   return {
     props: {
@@ -97,14 +107,14 @@ export async function getStaticProps({ params }: Params) {
 }
 
 export async function getStaticPaths() {
-  const posts = await getAllPosts(['slug'])
+  const posts = await getAllPosts(["slug"])
   return {
     paths: posts.map((post) => {
       return {
         params: {
           slug: post.slug.split(path.sep),
         },
-      } 
+      }
     }),
     fallback: false,
   }
